c105b885d5d044c23e2a512ba29965d931d7b5c0
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-06-07 14:35:58 -0700

    Added Home component
    included Featured Story placeholder
    will come from db eventually
    also added a router in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar.js"
 import Header from "./components/Header/Header.js"
-
-// import Home from "./components/Home/Home.js"
+import Home from "./components/Home/Home.js"
 
 // import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Navbar />
-      </div>
-    );
-  }
-}
+const App = () =>
+  <Router>
+    <div>
+      <Header />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* <Route exact path="/saved" component={Saved} /> */}
+      </Switch>
+    </div>
+  </Router>;
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Home = () => 
-    <div class="page-header">
-        <h1>Welcome to Myna <small>A place for shring and discovering new authors and short stories</small></h1>
-    </div>
+<div class="panel panel-default">
+<div class="panel-heading">
+  <h3 class="panel-title">Fear <small>by Lydia Davis</small></h3>
+</div>
+<div class="panel-body">
+Nearly every morning, a certain woman in our community comes running out of her house with her face white and her overcoat flapping wildly. She cries out, “Emergency, emergency,” and one of us runs to her and holds her until her fears are calmed. We know she is making it up; nothing has really happened to her. But we understand, because there is hardly one of us who has not been moved at some time to do just what she has done, and every time, it has taken all our strength, and even the strength of our friends and families too, to quiet us.
+</div>
+</div>
 
 
 export default Home;
