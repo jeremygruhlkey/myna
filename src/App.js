0bbff0837c840b2bcbb7c2d4e37c7c6d7bc9e859
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-06-07 14:15:03 -0700

    Added Header component
    placed above Navbar in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 import Navbar from "./components/Navbar/Navbar.js"
-// import logo from './logo.svg';
+import Header from "./components/Header/Header.js"
+
+// import Home from "./components/Home/Home.js"
+
 // import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Navbar />
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Welcome to Myna</h1>
-        </header>
-        <p className="App-intro">
-          A place to discover new authors and great short stories.
-        </p>
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => 
+    <div class="page-header">
+        <h1>Welcome to Myna <small>A place for sharing and discovering new authors and short stories</small></h1>
+    </div>
+
+
+export default Header;
